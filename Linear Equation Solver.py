18302d2cd824e2a63e9ebb895b31ff969ead3430
VedantChristian16 <55919296+VedantChristian16@users.noreply.github.com>
2020-03-30 20:47:40 +0100

    Add files via upload
    This is some code programmed in Python for a Linear Equation Solver.

---
+++ Linear Equation Solver.py
@@ -0,0 +1,20 @@
+'''
+Created by Vedant Christian
+Created on 30 / 03 / 2020
+'''
+import math
+
+a = int(input("Enter the coefficient of x. "))
+b = int(input("Enter the constant in the equation. "))
+if int(b) < 0:
+    b = b * -1
+    c = b / a
+    d = str(a) + "x"
+    print(d,"-", b,"= 0")
+    print("Root: ", c)
+else:
+    e = b / a
+    f = str(a) + "x"
+    print(f,"+", b,"= 0")
+    print("Root: ", (e * -1))
+input("")
